5074bf35d934d59c8c2d31b21b661d04ced2d759
Jennifer Aguirre <jenniferaguirre@Jennifers-MacBook-Air.local>
2019-10-05 12:26:17 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,50 @@
-
-// Array with the alphabet for the computer choice
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var computerGuess= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+var guessesLeft = 9;
 
-// Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
+
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
+var userGuessText = document.getElementById("userguess-text");
+var computerGuessText = document.getElementById("computerguess-text");
+var guessesLeft = document.getElementById("guessleft-text");
+
+
 
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
+    var userGuessLower = userGuess.toLowerCase();
+    
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-
-    if ((userGuess === computerChoices))
-
-
-        if ((userGuess === "a") || (userGuess === "b") || (userGuess === "")) {
-
-            if ((userGuess === "r" && computerGuess === "s") ||
-                (userGuess === "s" && computerGuess === "p") ||
-                (userGuess === "p" && computerGuess === "r")) {
-                wins++;
-            } else if (userGuess === computerGuess) {
-                ties++;
-            } else {
-                losses++;
-            }
+    var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
 
 
+    //this is where the choices, wins, loses and attempts left.
 
-            winsText.textContent = "Wins : " + wins;
-            lossesText.textContent = "Losses : " + losses;
-            tiesText.textContent = "Guesses left :" + guessesLeft;
-            userChoiceText.textContent = "Your Guesses so far:  " + userGuess;
+    // if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c")) {
 
+    if (userGuess === computerGuess) {
+        wins++;
+    }
+    else if (userGuess !== computerGuess) {
+        guessesLeft--;
+        losses++;
+    }
+
+    else if (userGuess = 0) {
+        wins = 0;
+        losses = 0;
+        guessesLeft = 0;
+
+
+    }
+
+    winsText.textContent = "Wins : " + wins;
+    lossesText.textContent = "Losses : " + losses;
+    guessesLeft.textContent = "Guesses left :" + guessesLeft;
+    userGuessText.textContent = "Your Guesses so far:  " + userGuess;
