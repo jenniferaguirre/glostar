ba83d1fd1615c28a0fbd0ac2fb6ed13d32209c36
Jennifer Aguirre <jenniferaguirre@Jennifers-MacBook-Air.local>
2019-10-06 18:39:07 -0400

    files updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,62 @@
-var computerGuess= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessedLetters = [];
 
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var userGuessText = document.getElementById("userguess-text");
-var computerGuessText = document.getElementById("computerguess-text");
-var guessesLeft = document.getElementById("guessleft-text");
+//var userGuessText = document.getElementById("userguess-text");
+// var computerGuessText = document.getElementById("computerguess-text");
+var guessesLeftText = document.getElementById("guessesleft-text");
+var guessedLettersText = document.getElementById("guessedletters-text");
 
 
-
+var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
     var userGuessLower = userGuess.toLowerCase();
-    
-
-    var computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
+    guessedLetters.push(userGuess);
 
 
 
-    //this is where the choices, wins, loses and attempts left.
+    if (computerGuess.indexOf(userGuess)) {
 
-    // if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c")) {
+        if (userGuess === computerGuess) {
+            wins++;
+        }
+        else if (userGuess !== computerGuess) {
+            guessesLeft--;
 
-    if (userGuess === computerGuess) {
-        wins++;
-    }
-    else if (userGuess !== computerGuess) {
-        guessesLeft--;
-        losses++;
-    }
+            winsText.textContent = "Wins : " + wins;
+            lossesText.textContent = "Losses : " + losses;
+            guessesLeftText.textContent = "Guesses left :" + guessesLeft;
+            //userGuessText.textContent = "Your Guesses so far:  " + userGuess;
+            guessedLettersText.textContent = "Your guesses so far: " + guessedLetters;
 
-    else if (userGuess = 0) {
-        wins = 0;
-        losses = 0;
-        guessesLeft = 0;
+        };
+        if (guessesLeft < 0) {
+            losses++;
+            alert("You didn't guessed what I'm thinking");
+            var playAgain = confirm("You lost. Press OK to play again")
+            if (playAgain === true) {
+                guessesLeft = 9;
+                guessedLetters = [];
+                computerGuess = computerGuess[math.floor(Math.random() * computerGuess.length)];
+
+            }
+
+        }
+
+    };
 
 
-    }
 
-    winsText.textContent = "Wins : " + wins;
-    lossesText.textContent = "Losses : " + losses;
-    guessesLeft.textContent = "Guesses left :" + guessesLeft;
-    userGuessText.textContent = "Your Guesses so far:  " + userGuess;
+
+
+
+};
