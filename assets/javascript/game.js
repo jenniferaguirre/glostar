87a7ea84c9cc4172c7dd0f782a9fca5fe8c98aa1
Jennifer Aguirre <jenniferaguirre@Jennifers-MacBook-Air.local>
2019-10-06 21:14:44 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,9 @@ var losses = 0;
 var guessesLeft = 9;
 var guessedLetters = [];
 
-
+var toStartText = document.getElementById("tostart-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-//var userGuessText = document.getElementById("userguess-text");
-// var computerGuessText = document.getElementById("computerguess-text");
 var guessesLeftText = document.getElementById("guessesleft-text");
 var guessedLettersText = document.getElementById("guessedletters-text");
 
@@ -28,18 +26,21 @@ document.onkeyup = function (event) {
 
         if (userGuess === computerGuess) {
             wins++;
+            guessesLeft = 9;
+                guessedLetters = [];
+                computerGuess = computerGuess[Math.floor(Math.random() * computerGuess.length)];
         }
         else if (userGuess !== computerGuess) {
             guessesLeft--;
 
+            toStartText.textContent = "";
             winsText.textContent = "Wins : " + wins;
             lossesText.textContent = "Losses : " + losses;
             guessesLeftText.textContent = "Guesses left :" + guessesLeft;
-            //userGuessText.textContent = "Your Guesses so far:  " + userGuess;
             guessedLettersText.textContent = "Your guesses so far: " + guessedLetters;
 
         };
-        if (guessesLeft < 0) {
+        if (guessesLeft === 0) {
             losses++;
             alert("You didn't guessed what I'm thinking");
             var playAgain = confirm("You lost. Press OK to play again")
